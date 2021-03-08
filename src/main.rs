ad2ad543b03597c21256f34c2ee34af6bda3301b
Scott A. Idler <scott.a.idler@gmail.com>
2021-03-07 23:38:09 -0800

    it compiles

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,51 @@
 use anyhow::Result;
+use anyhow::anyhow;
 use log::{info, warn};
 use std::env;
+
+use std::path::{Path, PathBuf};
+use structopt::StructOpt; //FIXME: consider parsing by hand
 
 pub mod cfg;
 use cfg::loader::Loader;
 
+const CONFIGS: &'static [&'static str] = &[
+    "~/.config/aka/aka.yml",
+    "~/.aka.yml",
+    "./aka.yml",
+];
+
+#[derive(Debug, StructOpt)]
+#[structopt(name = "aka", about = "[a]lso [k]nown [a]s: an aliasing program")]
+struct Args {
+    #[structopt(short, long)]
+    config: Option<PathBuf>,
+
+    cmdline: String,
+}
+
+fn test_path(config: Option<PathBuf>) -> Option<PathBuf> {
+    if let Some(file) = config {
+        if file.exists() {
+            return Some(file)
+        }
+    }
+    None
+}
+
+fn divine_config() -> Option<PathBuf> {
+    let configs: Vec<PathBuf> = CONFIGS.into_iter().map(|x| PathBuf::from(x)).collect();
+    for config in configs {
+        if config.exists() {
+            return Some(PathBuf::from(config))
+        }
+    }
+    None
+}
+
 fn main() -> Result<()> {
-    let args: Vec<String> = env::args().skip(1).collect();
+    let args = Args::from_args();
+
     println!("args: {:#?}", args);
 
     let verbose = true;
