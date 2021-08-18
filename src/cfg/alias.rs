a0f4003cdff796fc4ab43a0aeb801cf161a59664
Scott Idler <scott.a.idler@gmail.com>
2021-08-18 13:56:43 -0700

    added 'ls' and 'query' subcommands; refactored
    - added subcommands to allow for 'ls' (listing) aliases
    - refactored to put replace on the Alias instance

--- src/cfg/alias.rs
+++ src/cfg/alias.rs
@@ -37,15 +37,48 @@ impl Alias {
             .sorted()
             .collect()
     }
+
     pub fn keywords(&self) -> Vec<String> {
         let re = Regex::new(r"(\$[A-z]+)").unwrap();
         re.find_iter(&self.value)
             .filter_map(|m| m.as_str().parse().ok())
             .unique()
+            .sorted()
             .collect()
     }
+
     pub fn is_variadic(&self) -> bool {
         self.value.contains("$@")
+    }
+
+    fn replace_positionals(&self, remainders: &mut Vec<String>, result: &mut String) -> usize {
+        if self.positionals().len() > 0 {
+            if self.positionals().len() <= remainders.len() {
+                for positional in self.positionals().iter() {
+                    *result = result.replace(positional, &remainders.swap_remove(0));
+                }
+            }
+            return self.positionals().len();
+        }
+        0
+    }
+
+    fn replace_variadic(&self, remainders: &mut Vec<String>, result: &mut String) -> usize {
+        if self.is_variadic() {
+            *result = result.replace("$@", &remainders.join(" "));
+            let count: usize = remainders.len();
+            remainders.drain(0..remainders.len());
+            return count;
+        }
+        0
+    }
+
+    pub fn replace(&self, remainders: &mut Vec<String>) -> (String, usize) {
+        let mut result = self.value.to_owned();
+        let mut count = 0;
+        count += self.replace_positionals(remainders, &mut result);
+        count += self.replace_variadic(remainders, &mut result);
+        (result, count)
     }
 }
 
--- src/main.rs
+++ src/main.rs
@@ -18,21 +18,6 @@ const CONFIGS: &'static [&'static str] = &[
     "~/.config/aka/aka.yml",
 ];
 
-#[derive(Debug, StructOpt)]
-#[structopt(name = "aka", about = "[a]lso [k]nown [a]s: an aliasing program")]
-struct Args {
-    #[structopt(short, long)]
-    config: Option<PathBuf>,
-
-    #[structopt(short, long, help = "is entry an [e]nd [o]f [l]ine?")]
-    eol: bool,
-
-    #[structopt(short, long)]
-    verbose: bool,
-
-    cmdline: String,
-}
-
 fn divine_config() -> Result<PathBuf> {
     let configs: Vec<PathBuf> = CONFIGS
         .into_iter()
@@ -54,15 +39,51 @@ fn test_config(file: &PathBuf) -> Result<PathBuf> {
     Err(anyhow!("config {:?} not found!", file))
 }
 
+#[derive(Debug, StructOpt)]
+#[structopt(name = "aka", about = "[a]lso [k]nown [a]s: an aliasing program")]
+struct AkaOpts {
+    #[structopt(short, long, help = "is entry an [e]nd [o]f [l]ine?")]
+    eol: bool,
+
+    #[structopt(short, long)]
+    config: Option<PathBuf>,
+
+    #[structopt(short = "v",  parse(from_occurrences))]
+    verbosity: u8,
+
+    // SUBCOMMANDS
+    #[structopt(subcommand)]
+    command: Option<Command>
+}
+
+#[derive(StructOpt, Debug)]
+#[structopt(name = "comand", about = "choose command to run")]
+enum Command {
+    #[structopt(name = "ls", about = "list aka aliases")]
+    List(ListOpts),
+
+    #[structopt(name = "query", about = "query for aka substitutions")]
+    Query(QueryOpts),
+}
+
+#[derive(Debug, StructOpt)]
+struct QueryOpts {
+    cmdline: String,
+}
+
+#[derive(Debug, StructOpt)]
+struct ListOpts {
+    params: Vec<String>
+}
+
 #[derive(Debug)]
 struct AKA {
-    pub spec: Spec,
-    pub cmdline: String,
     pub eol: bool,
+    pub spec: Spec,
 }
 
 impl AKA {
-    pub fn new(cmdline: &String, eol: bool, config: Option<PathBuf>) -> Result<Self> {
+    pub fn new(eol: bool, config: Option<PathBuf>) -> Result<Self> {
         let config = match &config {
             Some(file) => test_config(file)?,
             None => divine_config()?,
@@ -71,12 +92,14 @@ impl AKA {
         let spec = loader.load(&config).unwrap();
         Ok(Self {
             spec,
-            cmdline: cmdline.to_owned(),
             eol,
         })
     }
-    pub fn use_alias(alias: &Alias, pos: usize) -> bool {
-        if pos == 0 {
+    pub fn use_alias(&self, alias: &Alias, pos: usize) -> bool {
+        if alias.is_variadic() && !self.eol {
+            false
+        }
+        else if pos == 0 {
             true
         }
         else if alias.global {
@@ -87,37 +110,24 @@ impl AKA {
         }
     }
 
-    pub fn replace(&self) -> Result<String> {
-        let mut args = split(&self.cmdline).unwrap_or(vec![]);
+    pub fn replace(&self, cmdline: &String) -> Result<String> {
+        let mut pos: usize = 0;
         let mut space = " ";
         let mut replaced = false;
-        let mut pos: usize = 0;
+        let mut args = split(&cmdline).unwrap_or(vec![]);
         while pos < args.len() {
             let arg = &args[pos];
-            let remainders: Vec<String> = args[pos+1..].to_vec();
-            let value = match self.spec.aliases.get(arg) {
-                Some(alias) if AKA::use_alias(&alias, pos) => {
+            let mut remainders: Vec<String> = args[pos+1..].to_vec();
+            let (value, count) = match self.spec.aliases.get(arg) {
+                Some(alias) if self.use_alias(&alias, pos) => {
                     replaced = true;
                     space = if alias.space { " " } else { "" };
-                    let positionals = alias.positionals();
-                    let _keywords = alias.keywords();
-                    if positionals.len() == remainders.len() {
-                        let mut result = alias.value.to_owned();
-                        let zipped = positionals.iter().zip(remainders.iter());
-                        for (positional, value) in zipped {
-                            result = result.replace(positional, value);
-                        }
-                        args.drain(pos+1..pos+1+positionals.len());
-                        result
-                    }
-                    else {
-                        replaced = false;
-                        arg.to_owned()
-                    }
+                    alias.replace(&mut remainders)
                 },
-                Some(_) => arg.to_owned(),
-                None => arg.to_owned(),
+                Some(_) => (arg.to_owned(), 0),
+                None => (arg.to_owned(), 0),
             };
+            args.drain(pos+1..pos+1+count);
             args[pos] = value;
             pos += 1;
         }
@@ -131,11 +141,23 @@ impl AKA {
 }
 
 fn execute() -> Result<i32> {
-    let args = Args::from_args();
-    let aka = AKA::new(&args.cmdline, args.eol, args.config)?;
-    println!("aka.eol={:?}", aka.eol);
-    let result = aka.replace()?;
-    println!("{}", result);
+    let aka_opts = AkaOpts::from_args();
+    let aka = AKA::new(aka_opts.eol, aka_opts.config)?;
+    if let Some(command) = aka_opts.command {
+        match command {
+            Command::Query(query_opts) => {
+                let result = aka.replace(&query_opts.cmdline)?;
+                println!("{}", result);
+            },
+            Command::List(_list_opts) => {
+                let mut aliases: Vec<Alias> = aka.spec.aliases.values().cloned().collect();
+                aliases.sort_by_key(|a| a.name.to_owned() );
+                for alias in aliases {
+                    println!("{}: {}", alias.name, alias.value);
+                }
+            },
+        }
+    }
     Ok(0)
 }
 
