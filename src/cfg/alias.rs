e290a4d26be5df80e4b02e815b99b917ed3657af
Scott A. Idler <scott.a.idler@gmail.com>
2023-05-27 23:41:31 -0700

    added tests for alias.rs

--- src/cfg/alias.rs
+++ src/cfg/alias.rs
@@ -105,3 +105,126 @@ impl FromStr for Alias {
     }
 }
 
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_positionals() -> Result<()> {
+        let alias = Alias {
+            name: "alias1".to_string(),
+            value: "echo $1 $2".to_string(),
+            space: true,
+            global: false,
+        };
+
+        assert_eq!(alias.positionals()?, vec!["$1", "$2"]);
+        Ok(())
+    }
+
+    #[test]
+    fn test_keywords() -> Result<()> {
+        let alias = Alias {
+            name: "alias2".to_string(),
+            value: "echo $name $location".to_string(),
+            space: true,
+            global: false,
+        };
+
+        assert_eq!(alias.keywords()?, vec!["$location", "$name"]);
+        Ok(())
+    }
+
+    #[test]
+    fn test_is_variadic() {
+        let alias = Alias {
+            name: "alias3".to_string(),
+            value: "echo $@".to_string(),
+            space: true,
+            global: false,
+        };
+
+        assert!(alias.is_variadic());
+    }
+
+    #[test]
+    fn test_replace() -> Result<()> {
+        let alias = Alias {
+            name: "alias4".to_string(),
+            value: "echo $1 $2".to_string(),
+            space: true,
+            global: false,
+        };
+    
+        let mut remainders = vec!["Hello".to_string(), "World".to_string()];
+        assert_eq!(alias.replace(&mut remainders)?, ("echo Hello World".to_string(), 2));
+        assert_eq!(remainders, Vec::<String>::new());  // Corrected this line
+    
+        let alias_variadic = Alias {
+            name: "alias5".to_string(),
+            value: "echo $@".to_string(),
+            space: true,
+            global: false,
+        };
+    
+        let mut remainders_variadic = vec!["Hello".to_string(), "from".to_string(), "Rust".to_string()];
+        assert_eq!(alias_variadic.replace(&mut remainders_variadic)?, ("echo Hello from Rust".to_string(), 3));
+        assert_eq!(remainders_variadic, Vec::<String>::new());  // Corrected this line
+    
+        Ok(())
+    }
+
+    #[test]
+    fn test_from_str() -> Result<()> {
+        let s = "echo Hello World";
+        let alias = s.parse::<Alias>()?;
+        assert_eq!(alias.name, "");
+        assert_eq!(alias.value, s);
+        assert!(alias.space);
+        assert!(!alias.global);
+        Ok(())
+    }
+   
+    #[test]
+    fn test_no_arguments() -> Result<()> {
+        let alias = Alias {
+            name: "alias".to_string(),
+            value: "echo Hello World".to_string(),
+            space: true,
+            global: false,
+        };
+    
+        assert_eq!(alias.positionals()?, Vec::<String>::new());
+        assert_eq!(alias.keywords()?, Vec::<String>::new());
+        Ok(())
+    }
+   
+    #[test]
+    fn test_replace_no_positional_with_variadic() -> Result<()> {
+        let alias = Alias {
+            name: "alias".to_string(),
+            value: "echo $@".to_string(),
+            space: true,
+            global: false,
+        };
+    
+        let mut remainders = vec!["Hello".to_string(), "World".to_string()];
+        assert_eq!(alias.replace(&mut remainders)?, ("echo Hello World".to_string(), 2));
+        assert_eq!(remainders, Vec::<String>::new());
+        Ok(())
+    }
+   
+    #[test]
+    fn test_replace_mismatch_remainders() -> Result<()> {
+        let alias = Alias {
+            name: "alias".to_string(),
+            value: "echo $1 $2 $3".to_string(),
+            space: true,
+            global: false,
+        };
+    
+        let mut remainders = vec!["Hello".to_string(), "World".to_string()];
+        assert_eq!(alias.replace(&mut remainders)?, ("alias".to_string(), 0)); // Alias name is returned when not enough arguments.
+        Ok(())
+    }
+}
