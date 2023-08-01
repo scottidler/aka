30c43fc213c38ddbff8864d17f58d115dafc301d
Scott A. Idler <scott.a.idler@gmail.com>
2023-07-31 22:18:14 -0700

    added the use of the GIT_DESCRIBE file to compare when changes happen

--- build.rs
+++ build.rs
@@ -1,45 +1,32 @@
 use std::process::Command;
-use std::fs::File;
+use std::fs::{File, write};
 use std::io::Write;
 use std::path::Path;
 use std::env;
 
 fn main() {
-    println!("cargo:rerun-if-env-changed=GIT_DESCRIBE");
-    let git_describe = env::var("GIT_DESCRIBE").unwrap_or_else(|_| {
-        if git_exists() && in_git_repo() {
-            // Run `git describe` command
-            let output = Command::new("git")
-                .args(&["describe"])
-                .output()
-                .expect("Failed to execute `git describe`");
+    // Get the output of `git describe` or the GIT_DESCRIBE environment variable
+    let value = env::var("GIT_DESCRIBE").unwrap_or_else(|_| {
+        let output = Command::new("git")
+            .args(&["describe"])
+            .output()
+            .expect("Failed to execute `git describe`");
 
-            // Get the output as a string
-            String::from_utf8(output.stdout).expect("Not UTF-8")
-        } else {
-            String::from("unknown")
-        }
+        String::from_utf8(output.stdout).expect("Not UTF-8")
     });
 
     // Write the output to a file
-    let out_dir = std::env::var("OUT_DIR").unwrap();
-    let dest_path = Path::new(&out_dir).join("git_describe.rs");
-    let mut f = File::create(&dest_path).unwrap();
+    let out_dir = env::var("OUT_DIR").unwrap();
+    let git_describe_rs = Path::new(&out_dir).join("git_describe.rs");
+    let mut f = File::create(&git_describe_rs).unwrap();
 
-    write!(f, "pub const GIT_DESCRIBE: &'static str = \"{}\";", git_describe).unwrap();
-}
+    write!(f, "pub const GIT_DESCRIBE: &'static str = \"{}\";", value).unwrap();
 
-fn git_exists() -> bool {
-    Command::new("git")
-        .args(&["--version"])
-        .output()
-        .is_ok()
-}
+    // Write the GIT_DESCRIBE environment variable to a GIT_DESCRIBE file
+    let git_describe = Path::new(&out_dir).join("GIT_DESCRIBE");
+    write(&git_describe, &value).unwrap();
 
-fn in_git_repo() -> bool {
-    Command::new("git")
-        .args(&["rev-parse", "--is-inside-work-tree"])
-        .output()
-        .map(|output| output.stdout == b"true\n")
-        .unwrap_or(false)
+    // Tell Cargo to rerun the build script if the .git/HEAD file or the GIT_DESCRIBE file changes
+    println!("cargo:rerun-if-changed=.git/HEAD");
+    println!("cargo:rerun-if-changed={}", git_describe.display());
 }
