d8c3de6f8caf78845918ed84aa55316b711e84b5
Scott A. Idler <scott.a.idler@gmail.com>
2023-08-01 19:18:40 -0700

    fixed up build.rs script to only build when a GIT_DESCRIBE value has changed

--- build.rs
+++ build.rs
@@ -1,32 +1,48 @@
 use std::process::Command;
-use std::fs::{File, write};
+use std::fs::{File, write, read_to_string};
 use std::io::Write;
 use std::path::Path;
 use std::env;
 
-fn main() {
+fn git_describe_value() -> String {
     // Get the output of `git describe` or the GIT_DESCRIBE environment variable
-    let value = env::var("GIT_DESCRIBE").unwrap_or_else(|_| {
+    env::var("GIT_DESCRIBE").unwrap_or_else(|_| {
         let output = Command::new("git")
             .args(&["describe"])
             .output()
             .expect("Failed to execute `git describe`");
 
         String::from_utf8(output.stdout).expect("Not UTF-8")
-    });
+    })
+}
 
-    // Write the output to a file
+fn main() {
+    // Get the path to the GIT_DESCRIBE file
     let out_dir = env::var("OUT_DIR").unwrap();
-    let git_describe_rs = Path::new(&out_dir).join("git_describe.rs");
-    let mut f = File::create(&git_describe_rs).unwrap();
-
-    write!(f, "pub const GIT_DESCRIBE: &'static str = \"{}\";", value).unwrap();
-
-    // Write the GIT_DESCRIBE environment variable to a GIT_DESCRIBE file
     let git_describe = Path::new(&out_dir).join("GIT_DESCRIBE");
-    write(&git_describe, &value).unwrap();
+    println!("BUILD_RS: GIT_DESCRIBE file is located at: {}", git_describe.display());
 
-    // Tell Cargo to rerun the build script if the .git/HEAD file or the GIT_DESCRIBE file changes
-    println!("cargo:rerun-if-changed=.git/HEAD");
-    println!("cargo:rerun-if-changed={}", git_describe.display());
+    // Read the old GIT_DESCRIBE value from the GIT_DESCRIBE file
+    let old_value = read_to_string(&git_describe).unwrap_or_default();
+
+    // Get the output of `git describe` or the GIT_DESCRIBE environment variable
+    let new_value = git_describe_value();
+
+    // If the new GIT_DESCRIBE value is different from the old one, write it to the GIT_DESCRIBE file
+    if new_value != old_value {
+        println!("BUILD_RS: old_value='{old_value}' != new_value='{new_value}'");
+
+        write(&git_describe, &new_value).unwrap();
+
+        // Write the output to a file
+        let git_describe_rs = Path::new(&out_dir).join("git_describe.rs");
+        let mut f = File::create(&git_describe_rs).unwrap();
+
+        write!(f, "pub const GIT_DESCRIBE: &'static str = \"{}\";", new_value).unwrap();
+
+        // Tell Cargo to rerun the build script if the GIT_DESCRIBE file changes
+        println!("cargo:rerun-if-changed={}", git_describe.display());
+    } else {
+        println!("BUILD_RS: old_value='{old_value}' == new_value='{new_value}'");
+    }
 }
