9ad13a3e79c189a66a2c05c2d27f7c8e70f74f4f
Scott A. Idler <scott.a.idler@gmail.com>
2023-08-18 15:42:06 -0700

    made some changes. Maybe this fixed it

--- build.rs
+++ build.rs
@@ -11,31 +11,27 @@ fn git_describe_value() -> String {
             .output()
             .expect("Failed to execute `git describe`");
 
-        String::from_utf8(output.stdout).expect("Not UTF-8")
+        String::from_utf8(output.stdout).expect("Not UTF-8").trim().to_string()
     })
 }
 
 fn main() {
     let out_dir = env::var("OUT_DIR").unwrap();
     let git_describe = Path::new(&out_dir).join("GIT_DESCRIBE");
-    println!("BUILD_RS: GIT_DESCRIBE file is located at: {}", git_describe.display());
 
-    let old_value = read_to_string(&git_describe).unwrap_or_default();
-
+    let old_value = read_to_string(&git_describe)
+        .unwrap_or_default()
+        .trim()
+        .to_string();
     let new_value = git_describe_value();
 
     if new_value != old_value {
-        println!("BUILD_RS: old_value='{old_value}' != new_value='{new_value}'");
-
         write(&git_describe, &new_value).unwrap();
 
         let git_describe_rs = Path::new(&out_dir).join("git_describe.rs");
         let mut f = File::create(&git_describe_rs).unwrap();
-
         write!(f, "pub const GIT_DESCRIBE: &'static str = \"{}\";", new_value).unwrap();
-
-        println!("cargo:rerun-if-changed={}", git_describe.display());
-    } else {
-        println!("BUILD_RS: old_value='{old_value}' == new_value='{new_value}'");
     }
+
+    println!("cargo:rerun-if-env-changed=GIT_DESCRIBE");
 }
