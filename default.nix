866237b23113dde8c6c5526fd5bdf7825c10f017
Scott A. Idler <scott.a.idler@gmail.com>
2024-03-31 11:27:08 -0700

    added autoPatchelfHook

--- default.nix
+++ default.nix
@@ -1,24 +1,23 @@
 # default.nix
 
-{ stdenv, fetchurl, lib, ... }:
+{ stdenv, fetchurl, autoPatchelfHook, glibc, lib, ... }:
 
 let
   version = "0.3.15";
   owner = "scottidler";
   repo = "aka";
   suffix = "linux"; # Adjust based on the target OS, e.g., "macos" for macOS builds
-
-  # URL and sha256 for the release tarball
   tarball = fetchurl {
     url = "https://github.com/${owner}/${repo}/releases/download/v${version}/aka-v${version}-${suffix}.tar.gz";
-    sha256 = "082wk31b2ybs63rxib7ym54jly4ywwiyiz7shnxda18hl0ijsrxd"; # Use `nix-prefetch-url` to obtain this
+    sha256 = "082wk31b2ybs63rxib7ym54jly4ywwiyiz7shnxda18hl0ijsrxd";
   };
-
 in stdenv.mkDerivation rec {
   pname = "aka";
   inherit version;
 
   src = tarball;
+
+  nativeBuildInputs = [ autoPatchelfHook ];
 
   dontBuild = true;
 
